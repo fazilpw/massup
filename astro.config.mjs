b204aed00b9e4b5e66f38078ef66be0d6dabacc1
fazilpw <fazillatam@gmail.com>
2025-07-08 19:20:54 -0500

    “Astro679”

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,20 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
+import vercel from "@astrojs/vercel/static";
+
 // https://astro.build/config
 export default defineConfig({
+  output: "static",
+  adapter: vercel({
+    webAnalytics: { enabled: true }
+  }),
   vite: {
     plugins: [tailwindcss()],
   },
+  site: "https://mass-up.vercel.app", // Cambia por tu dominio final
+  compressHTML: true,
+  build: {
+    inlineStylesheets: "auto"
+  }
 });
